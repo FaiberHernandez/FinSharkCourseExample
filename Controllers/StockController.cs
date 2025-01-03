d36803bc8977cd9d20e3ad088e148cd3956ecc2d
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-03 15:36:46 -0500

    branch creation

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -16,7 +17,7 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stock.ToList();
+            var stocks = _context.Stock.ToList().Select(stock => stock.ToStockDto());
             return Ok(stocks);
         }
 
@@ -28,7 +29,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
         
     }
