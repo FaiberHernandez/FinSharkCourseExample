7a7764a8d3d2e4a9147a8409c5a60ebdaed2004a
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-03 16:39:31 -0500

    saving changes, sotck dto and mapper

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,6 +31,15 @@ namespace api.Controllers
                 return NotFound();
             }
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFormCreateDTO();
+            _context.Stock.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
         
     }
