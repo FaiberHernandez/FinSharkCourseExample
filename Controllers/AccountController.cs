7573afbb106e0fc36ab41355b5bc324800c1ccac
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-05 16:43:29 -0500

    reset

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using api.Dtos.Account;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController: ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerDto)
+        {
+            try
+            {
+                if(!ModelState.IsValid) return BadRequest(ModelState);
+
+                var newUser = new AppUser
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email
+                };
+
+                var createdUser = await _userManager.CreateAsync(newUser, registerDto.Password);
+
+                if(createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(newUser, "User");
+                    if(roleResult.Succeeded)
+                    {
+                        return Ok("User created successfully");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,17 @@ namespace api.Data
 
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole {Id = "Admin", Name = "Admin", NormalizedName = "ADMIN", ConcurrencyStamp = Guid.NewGuid().ToString()},
+                new IdentityRole {Id = "User", Name = "User", NormalizedName = "USER", ConcurrencyStamp = Guid.NewGuid().ToString()}
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
 
     }
 }
