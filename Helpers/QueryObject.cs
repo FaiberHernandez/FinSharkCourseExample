7bb032d5531fc8378a68adb942f5201f671ef29c
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-05 14:14:14 -0500

    sorting example

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -4,5 +4,7 @@ namespace api.Helpers
     {
         public string? Symbol { get; set; }
         public string? CompanyName { get; set; }
+        public string? SortBy { get; set; }
+        public bool IsDescending { get; set; }
     }
 }
