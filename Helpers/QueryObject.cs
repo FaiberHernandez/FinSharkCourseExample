fdf0a9216222ba981357d6233ec7310b8ab28948
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-05 14:18:31 -0500

    basica pagination

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -6,5 +6,7 @@ namespace api.Helpers
         public string? CompanyName { get; set; }
         public string? SortBy { get; set; }
         public bool IsDescending { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
