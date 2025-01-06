a5935cc7a4ace891555d3189a8ae0fd39ef79b3c
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-05 19:07:22 -0500

    Login and refactor on registration endPoint

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -12,10 +13,34 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginDto)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName);
+
+            if(user == null) return Unauthorized("Invalid username");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized("Invalid username and/or password");
+
+            return Ok(new NewUserDto
+            {
+                UserName = user.UserName!,
+                Email = user.Email!,
+                Token = _tokenService.CreateToken(user)
+            });
         }
 
         [HttpPost("register")]
@@ -33,32 +58,22 @@ namespace api.Controllers
 
                 var createdUser = await _userManager.CreateAsync(newUser, registerDto.Password);
 
-                if(createdUser.Succeeded)
+                if(!createdUser.Succeeded) return StatusCode(500, createdUser.Errors);
+
+                var roleResult = await _userManager.AddToRoleAsync(newUser, "User");
+
+                if(!roleResult.Succeeded) return StatusCode(500, roleResult.Errors);
+                
+                return Ok(new NewUserDto
                 {
-                    var roleResult = await _userManager.AddToRoleAsync(newUser, "User");
-                    if(roleResult.Succeeded)
-                    {
-                        return Ok(new NewUserDto
-                        {
-                            UserName = newUser.UserName,
-                            Email = newUser.Email,
-                            Token = _tokenService.CreateToken(newUser)
-                        });
-                    }
-                    else
-                    {
-                        return StatusCode(500, roleResult.Errors);
-                    }
-                }
-                else
-                {
-                    return StatusCode(500, createdUser.Errors);
-                }
+                    UserName = newUser.UserName,
+                    Email = newUser.Email,
+                    Token = _tokenService.CreateToken(newUser)
+                });
                 
             }
             catch (Exception e)
             {
-                
                 return StatusCode(500, e);
             }
         }
