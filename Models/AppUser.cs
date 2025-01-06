43aed33e594bcda78ab7b595ff9fb42b9c50fa8f
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-05 19:41:58 -0500

    saving

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace api.Models
 {
+    [Table("AppUsers")]
     public class AppUser: IdentityUser
     {
-
+        public List<Portfolio> Portfolios { get; set; } = [];
     }
 }
